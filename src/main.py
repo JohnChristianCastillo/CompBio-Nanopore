5d689834cb22901d80d21e6a5fc62813c8b81039
Hayaan Hussein Abdi <hayaanhusseinabdi@gmail.com>
2022-05-31 19:29:40 +0200

    Refactor and add parsing of FastA files

--- src/main.py
+++ src/main.py
@@ -1,49 +1,76 @@
-COLOR_CODES = {
-    "2": "B",
-    "3": "G",
-    "4": "Y",
-    "5": "R",
-    "7": "",
-    "6": "",
-    "1": "",
+from typing import List
+import csv
+
+import editdistance
+
+DNA_SEQUENCES = [
+    "YGYBBGGRGGBRGRGGYBGGGBGGRRB",
+    "RBYBYBGBYBRRBBGRRGBBBYYRB",
+    "RRGRBGYGGRBYBGBBYRBRRB",
+    "BGRYGRBGRRGBRYRBBYYG",
+    "BRGYYGGBBGBGBYYYBGRGYYY",
+    "YYBRBBRBYRGGYYGYGRBGG",
+    "RBRBRBYBBGYGGGBBYRRGRR",
+]
+
+BASE_TO_SENSOR_VALUE = {
+    "B": "2",
+    "G": "3",
+    "Y": "4",
+    "R": "5",
 }
 
+NUM_SENSOR_VALUES_PER_BASE = 47
+NUM_STRINGS_PER_SEQUENCE = 5
+
+
+def generate_fake_reading_from_dna(dna: str) -> List:
+    sensor_values: List[int] = []
+    for base in dna:
+        if base not in BASE_TO_SENSOR_VALUE:
+            continue
+        sensor_values.extend([BASE_TO_SENSOR_VALUE[base]] * NUM_SENSOR_VALUES_PER_BASE)
+    return sensor_values
+
+
+def apply_mutations():
+    """
+    apply to vector (of signals) or DNA sequence?
+    :return:
+    """
+    return None
+
+
+def get_sensor_values_from_file(path: str) -> List[str]:
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        return [row["Colour"] for row in reader]
+
+
+def test_sequence(index):
+    # sequence we'll generate a fake signal from compare against our other signals
+    dna_sequence = DNA_SEQUENCES[index]
+    generated_sensor_readings = "".join(generate_fake_reading_from_dna(dna_sequence))
+    # print(generated_sensor_readings)
+
+    results = []
+    for sequence_index in range(1, len(DNA_SEQUENCES) + 1):
+        for reading_index in range(1, 4):
+            reading = "".join(
+                get_sensor_values_from_file(
+                    f"data/{sequence_index}_{reading_index}.csv"
+                )
+            )
+            # print(reading)
+            inverse_score = editdistance.distance(
+                reading, generated_sensor_readings
+            )  # FIXME: use a passed in scoring function param
+            results.append((sequence_index, inverse_score))
+    results.sort(key=lambda result: result[1])
+    return [result[0] for result in results]
+    # return results
+
+
 if __name__ == "__main__":
-    import csv
-
-    loc = 45
-    time = 0.010
-    timeBetweenBlocks = 0
-
-    output = ""
-    with open("../data/4_1.csv") as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
-        next(reader)  # skip first row
-
-        next(reader)  # skip next row
-        prev = 0
-        timeAfterTimeout = 0
-        for row in reader:
-            # if timer times out
-            if timeBetweenBlocks == 0:
-                timeBetweenBlocks = time * loc
-                output += COLOR_CODES[row[1][0]]
-                prev = row[1]
-            # if we encounter a new color
-            elif prev != row[1]:
-                # if we encounter it moments after we added one
-                if timeAfterTimeout < 0.020:
-                    output = output[:-1]  # remove last char
-                    # reset timer
-                    timeAfterTimeout = 0
-                # if we just encountered a new one
-                output += COLOR_CODES[row[1][0]]
-                timeBetweenBlocks = time * loc
-                # update the prev
-                prev = row[1]
-
-            timeBetweenBlocks -= 0.010
-            timeAfterTimeout += 0.010
-            # print(', '.join(row))
-
-    print(output)
+    for i in range(len(DNA_SEQUENCES)):
+        print(f"sequence {i + 1} matches (closest to furthest): {test_sequence(i)}")
