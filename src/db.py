5df32a0baa082173422f5c1894f4faacdd60f707
Hayaan Hussein Abdi <hayaanhusseinabdi@gmail.com>
2022-06-07 15:37:04 +0200

    Modify get_sensor_values_from_file to trim unneeded values

--- src/db.py
+++ src/db.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 
 from Bio import SeqIO
@@ -18,7 +19,9 @@ BASE_TO_SIGNAL = {
 }
 
 # TODO: calibrate this value by testing against the DNA sequences and CSV files we already have
-NUM_SENSOR_VALUES_PER_BASE = 47
+# NUM_SENSOR_VALUES_PER_BASE = 47
+NUM_SENSOR_VALUES_PER_BASE = 1
+
 
 def generate_vector_from_sequence(sequence: str) -> List[int]:
     """
@@ -42,8 +45,46 @@ def apply_mutations():
     return None
 
 
-if __name__ == '__main__':
-    for record in SeqIO.parse("data/animalBlast.fasta", "fasta"):
-        name, binomen = record.id.split('|')
-        sequence = record.seq
-        print(f"{name},{binomen},{sequence},{''.join(generate_vector_from_sequence(sequence))}")
+# num of 10s in a row, we want to see to ltrim
+# TODO: better name
+K: int = 50
+
+
+def get_sensor_values_from_file(path: str) -> list[int]:
+    import csv
+
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        values = [int(float(row["Colour_p4_01"])) for row in reader]
+    # rtrim values
+    # assumes it stops and continues reading 6 when the end of the LEGO sequence
+    # has been reached
+    for val in reversed(values):
+        # if white
+        if val == 6:
+            del values[-1]
+        else:
+            break
+    # run over list while reversed, once you encounter the first chunk of X 6's, slice from that index onwards
+    counter = Counter(values[-K:])
+    # not foolproof but should work for our usecases
+    for index in reversed(range(0, len(values) - K)):
+        if counter[6] == K:
+            print(f"index broken @ {index}")
+            return values[index + K + 1:]
+        counter[values[index + K]] -= 1
+        counter[values[index]] += 1
+    raise ValueError("unable to left trim white blocks from sequence of signals")
+
+
+if __name__ == "__main__":
+    sensor_values = get_sensor_values_from_file("./standard.csv")
+    print(len(sensor_values))
+    print(sensor_values)
+
+    # # for record in SeqIO.parse("data/animalBlast.fasta", "fasta"):
+    # for record in SeqIO.parse("data/extraAnimalBlast.fasta", "fasta"):
+    #     # name, binomen = record.id.split('|')
+    #     name = record.id
+    #     sequence = record.seq
+    #     print(f"{name},{sequence},{''.join(generate_vector_from_sequence(sequence))}")
